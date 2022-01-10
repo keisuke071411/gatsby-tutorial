4026903a47c89633d096c0d29c373d9d32c06b7e
keisuke071411 <i.love.football0714@gmail.com>
2022-01-10 23:28:24 +0900

    Add: styled by emotion

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+import "./src/styles/ress.css";
+import "./src/styles/global.css";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
 import { graphql, Link, PageProps } from "gatsby";
+import { Card } from "../components/Card";
+import { css } from "@emotion/react";
+
+const container = css`
+  max-width: 1024px;
+  margin: 0 auto;
+`;
+
+const grid = css`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 40px;
+`;
 
 export const query = graphql`
   query TopPage {
@@ -21,14 +34,15 @@ export const query = graphql`
 // markup
 const IndexPage = ({ data }: PageProps<GatsbyTypes.TopPageQuery>) => {
   return (
-    <main>
-      <p>hello gatsby</p>
-      <ul>
+    <main css={container}>
+      <ul css={grid}>
         {data.allMicrocmsBlogs.edges.map(({ node }) => (
           <li key={node.blogsId}>
-            <Link to={node.blogsId ?? "/"}>
-              {node?.title && <p>{node.title}</p>}
-            </Link>
+            <Card
+              title={node?.title ?? ""}
+              src={node?.thumbnail?.url ?? ""}
+              to={node.blogsId ?? "/"}
+            />
           </li>
         ))}
       </ul>
